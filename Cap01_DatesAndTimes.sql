3b462c478e2d310a586c8e1098d1fb3024dcdede
JulioQuispe-Chavez17 <53870854+JulioQuispe-Chavez17@users.noreply.github.com>
2020-08-17 17:12:10 -0500

    Primer capitulo terminado.

--- Cap01_DatesAndTimes.sql
+++ Cap01_DatesAndTimes.sql
@@ -5,3 +5,196 @@ SELECT
 	YEAR(@SomeTime) AS TheYear,
 	MONTH(@SomeTime) AS TheMonth,
 	DAY(@SomeTime) AS TheDay;
+-- Break a date and time into component parts(1)
+DECLARE
+	@BerlinWallFalls DATETIME2(7) = '1989-11-09 23:49:36.2294852';
+SELECT
+	DATEPART(year, @BerlinWallFalls) AS TheYear,
+	DATEPART(month, @BerlinWallFalls) AS TheMonth,
+	DATEPART(day, @BerlinWallFalls) AS TheDay,
+	DATEPART(dayofyear, @BerlinWallFalls) AS TheDayOfYear,
+	DATEPART(WEEKDAY, @BerlinWallFalls) AS TheDayOfWeek,
+	DATEPART(week, @BerlinWallFalls) AS TheWeek,
+	DATEPART(second, @BerlinWallFalls) AS TheSecond,
+	DATEPART(nanosecond, @BerlinWallFalls) AS TheNanosecond;
+-- Break a date and time into component parts(2)
+DECLARE
+	@BerlinWallFalls DATETIME2(7) = '1989-11-09 23:49:36.2294852';
+SELECT
+	DATENAME(YEAR, @BerlinWallFalls) AS TheYear,
+	DATENAME(MONTH, @BerlinWallFalls) AS TheMonth,
+	DATENAME(DAY, @BerlinWallFalls) AS TheDay,
+	DATENAME(DAYOFYEAR, @BerlinWallFalls) AS TheDayOfYear,
+	DATENAME(WEEKDAY, @BerlinWallFalls) AS TheDayOfWeek,
+	DATENAME(WEEK, @BerlinWallFalls) AS TheWeek,
+	DATENAME(SECOND, @BerlinWallFalls) AS TheSecond,
+	DATENAME(NANOSECOND, @BerlinWallFalls) AS TheNanosecond;
+-- Break a date and time into component parts(3): Possible Answers - Two
+-- Date math and leap years(1)
+DECLARE
+	@LeapDay DATETIME2(7) = '2012-02-29 18:00:00';
+SELECT
+	DATEADD(DAY, -1, @LeapDay) AS PriorDay,
+	DATEADD(DAY, 1, @LeapDay) AS NextDay,
+	DATEADD(YEAR, -4, @LeapDay) AS PriorLeapYear,
+	DATEADD(YEAR, 4, @LeapDay) AS NextLeapYear,
+	DATEADD(YEAR, -1, @LeapDay) AS PriorYear;
+-- Date math and leap years(2)
+DECLARE
+	@PostLeapDay DATETIME2(7) = '2012-03-01 18:00:00';
+SELECT
+	DATEADD(DAY, -1, @PostLeapDay) AS PriorDay,
+	DATEADD(DAY, 1, @PostLeapDay) AS NextDay,
+	DATEADD(YEAR, -4, @PostLeapDay) AS PriorLeapYear,
+	DATEADD(YEAR, 4, @PostLeapDay) AS NextLeapYear,
+	DATEADD(YEAR, -1, @PostLeapDay) AS PriorYear,
+	DATEADD(DAY, -1, DATEADD(YEAR, 4, @PostLeapDay)) AS NextLeapDay,
+    DATEADD(DAY, -2, @PostLeapDay) AS TwoDaysAgo;
+-- Date math and leap years(3)
+DECLARE
+	@PostLeapDay DATETIME2(7) = '2012-03-01 18:00:00',
+    @TwoDaysAgo DATETIME2(7);
+
+SELECT
+	@TwoDaysAgo = DATEADD(DAY, -2, @PostLeapDay);
+
+SELECT
+	@TwoDaysAgo AS TwoDaysAgo,
+	@PostLeapDay AS SomeTime,
+	DATEDIFF(DAY, @TwoDaysAgo, @PostLeapDay) AS DaysDifference,
+	DATEDIFF(HOUR, @TwoDaysAgo, @PostLeapDay) AS HoursDifference,
+	DATEDIFF(MINUTE, @TwoDaysAgo, @PostLeapDay) AS MinutesDifference;
+-- Rounding dates
+DECLARE
+	@SomeTime DATETIME2(7) = '2018-06-14 16:29:36.2248991';
+SELECT
+	DATEADD(DAY, DATEDIFF(DAY, 0, @SomeTime), 0) AS RoundedToDay,
+	DATEADD(HOUR, DATEDIFF(HOUR, 0, @SomeTime), 0) AS RoundedToHour,
+	DATEADD(MINUTE, DATEDIFF(MINUTE, 0, @SomeTime), 0) AS RoundedToMinute;
+-- Formatting dates with CAST() and CONVERT() (1)
+DECLARE
+	@CubsWinWorldSeries DATETIME2(3) = '2016-11-03 00:30:29.245',
+	@OlderDateType DATETIME = '2016-11-03 00:30:29.245';
+
+SELECT
+	-- Fill in the missing function calls
+	CAST(@CubsWinWorldSeries AS DATE) AS CubsWinDateForm,
+	CAST(@CubsWinWorldSeries AS NVARCHAR(30)) AS CubsWinStringForm,
+	CAST(@OlderDateType AS DATE) AS OlderDateForm,
+	CAST(@OlderDateType AS NVARCHAR(30)) AS OlderStringForm;
+-- Formatting dates with CAST() and CONVERT() (2)
+DECLARE
+	@CubsWinWorldSeries DATETIME2(3) = '2016-11-03 00:30:29.245';
+
+SELECT
+	CAST(CAST(@CubsWinWorldSeries AS DATE) AS NVARCHAR(30)) AS DateStringForm;
+-- Formatting dates with CAST() and CONVERT() (3)
+DECLARE
+	@CubsWinWorldSeries DATETIME2(3) = '2016-11-03 00:30:29.245';
+
+SELECT
+	CONVERT(DATE, @CubsWinWorldSeries) AS CubsWinDateForm,
+	CONVERT(NVARCHAR(30), @CubsWinWorldSeries) AS CubsWinStringForm;
+-- Formatting dates with CAST() and CONVERT() (4)
+DECLARE
+	@CubsWinWorldSeries DATETIME2(3) = '2016-11-03 00:30:29.245';
+
+SELECT
+	CONVERT(NVARCHAR(30), @CubsWinWorldSeries, 0) AS DefaultForm,
+	CONVERT(NVARCHAR(30), @CubsWinWorldSeries, 3) AS UK_dmy,
+	CONVERT(NVARCHAR(30), @CubsWinWorldSeries, 1) AS US_mdy,
+	CONVERT(NVARCHAR(30), @CubsWinWorldSeries, 103) AS UK_dmyyyy,
+	CONVERT(NVARCHAR(30), @CubsWinWorldSeries, 101) AS US_mdyyyy;
+-- Formatting dates with FORMAT() (1)
+DECLARE
+	@Python3ReleaseDate DATETIME2(3) = '2008-12-03 19:45:00.033';
+
+SELECT
+	-- Fill in the function call and format parameter
+	FORMAT(@Python3ReleaseDate, 'd', 'en-US') AS US_d,
+	FORMAT(@Python3ReleaseDate, 'd', 'de-DE') AS DE_d,
+	-- Fill in the locale for Japan
+	FORMAT(@Python3ReleaseDate, 'd', 'jp-JP') AS JP_d,
+	FORMAT(@Python3ReleaseDate, 'd', 'zh-cn') AS CN_d;
+-- Formatting dates with FORMAT() (2)
+DECLARE
+	@Python3ReleaseDate DATETIME2(3) = '2008-12-03 19:45:00.033';
+
+SELECT
+	-- Fill in the format parameter
+	FORMAT(@Python3ReleaseDate, 'D', 'en-US') AS US_D,
+	FORMAT(@Python3ReleaseDate, 'D', 'de-DE') AS DE_D,
+	-- Fill in the locale for Indonesia
+	FORMAT(@Python3ReleaseDate, 'D', 'id-ID') AS ID_D,
+	FORMAT(@Python3ReleaseDate, 'D', 'zh-cn') AS CN_D;
+-- Formatting dates with FORMAT() (3)
+DECLARE
+	@Python3ReleaseDate DATETIME2(3) = '2008-12-03 19:45:00.033';
+    
+SELECT
+	-- 20081203
+	FORMAT(@Python3ReleaseDate, 'yyyyMMdd') AS F1,
+	-- 2008-12-03
+	FORMAT(@Python3ReleaseDate, 'yyyy-MM-dd') AS F2,
+	-- Dec 03+2008 (the + is just a "+" character)
+	FORMAT(@Python3ReleaseDate, 'MMM dd+yyyy') AS F3,
+	-- 12 08 03 (month, two-digit year, day)
+	FORMAT(@Python3ReleaseDate, 'MM yy dd') AS F4,
+	-- 03 Dec 07:45 2008.00
+    -- (day, hour, minute, year, ".", second)
+	FORMAT(@Python3ReleaseDate, 'dd HH:mm yyyy.ss') AS F5;
+ -- The benefits of calendar tables: Solutions Calendar tables can simplify queries which perform complicated date math
+ -- Try out a calendar table(1)
+ SELECT
+	c.Date
+FROM dbo.Calendar c
+WHERE
+	c.MonthName = 'December'
+	AND c.DayName = 'Tuesday'
+	AND c.CalendarYear BETWEEN 2008 AND 2010
+ORDER BY
+	c.Date;
+-- Try out a calendar table(2)
+SELECT
+	c.Date
+FROM dbo.Calendar c
+WHERE
+    -- Instead of month, use the fiscal week
+	c.FiscalWeekOfYear = 29
+    -- Instead of calendar year, use fiscal year
+	AND c.FiscalYear = 2019
+ORDER BY
+	c.Date ASC;
+ -- Joining to a calendar table(1)
+ SELECT
+	ir.IncidentDate,
+	c.FiscalDayOfYear,
+	c.FiscalWeekOfYear
+FROM dbo.IncidentRollup ir
+	INNER JOIN dbo.Calendar c
+		ON ir.IncidentDate = c.Date
+WHERE
+    -- Incident type 3
+	ir.IncidentTypeID = 3
+    -- Fiscal year 2019
+	AND c.FiscalYear = 2019
+    -- Fiscal quarter 3
+	AND c.FiscalQuarter = 3;
+ -- Joining to a calendar table(2)
+ SELECT
+	ir.IncidentDate,
+	c.FiscalDayOfYear,
+	c.FiscalWeekOfYear
+FROM dbo.IncidentRollup ir
+	INNER JOIN dbo.Calendar c
+		ON ir.IncidentDate = c.Date
+WHERE
+    -- Incident type 4
+	ir.IncidentTypeID = 4
+    -- Fiscal year 2019
+	AND c.FiscalYear = 2019
+    -- Beyond fiscal week of year 30
+	AND c.FiscalWeekOfYear > 30
+    -- Only return weekends
+	AND c.IsWeekend = 1;
+ 
